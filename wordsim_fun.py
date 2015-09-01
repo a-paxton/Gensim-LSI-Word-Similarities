bf630fb66cf10cbcdb438041bdc6cb61250b9684
Alexandra Paxton <paxton.alexandra@gmail.com>
2015-08-31 23:05:47 -0700

    Upload function
    Upload function.

---
+++ wordsim_fun.py
@@ -0,0 +1,23 @@
+# preliminaries
+import pandas as pd
+import scipy
+import gensim
+
+# define our function
+def wordsim(word1,word2,target_dictionary,target_lsi_model):
+    
+    # look up each word in the LSA/LSI model of choice
+    vec_bow1 = target_dictionary.doc2bow([word1])
+    vec_bow2 = target_dictionary.doc2bow([word2])
+    
+    # make sure both words are actually in the topic space
+    if len(vec_bow1) > 0 and len(vec_bow2) > 0:
+        
+        # if they are, go ahead and find their values in the "num_topic"-dimensional space created from gensim.models.LsiModel
+        vec_lsi1 = pd.DataFrame(target_lsi_model[vec_bow1],columns=['dim','val'])
+        vec_lsi2 = pd.DataFrame(target_lsi_model[vec_bow2],columns=['dim','val'])
+        return -1*scipy.spatial.distance.cosine(vec_lsi1['val'],vec_lsi2['val'])+1
+    
+    # if the word isn't in the topic space, kick back an error
+    else:
+        raise RuntimeError('Word not found in list.')

85c5f8231828c6de3d8bed026d91c63e2adc5301
Alexandra Paxton <paxton.alexandra@gmail.com>
2015-09-02 21:44:01 -0700

    Updated comments
    Acknowledging Rick Dale’s contribution of a snippet of similarity
    cosine code in both functions

--- wordvectsim_fun.py
+++ wordvectsim_fun.py
@@ -19,7 +19,7 @@ def wordvectsim(word_vector2d,target_dictionary,target_lsi_model):
         # if they are, go ahead and find their values in the "num_topic"-dimensional space created from gensim.models.LsiModel
         vec_lsi1 = pd.DataFrame(target_lsi_model[vec_bow1],columns=['dim','val'])
         vec_lsi2 = pd.DataFrame(target_lsi_model[vec_bow2],columns=['dim','val'])
-        return -1*scipy.spatial.distance.cosine(vec_lsi1['val'],vec_lsi2['val'])+1
+        return -1*scipy.spatial.distance.cosine(vec_lsi1['val'],vec_lsi2['val'])+1 # snippet from Rick Dale 
     
     # if the word isn't in the topic space, kick back an error
     else:
